e36ebdce38faa355cf9a6ce2aab988cc9394bfe6
Herin Kim <herin@hyperlootproject.com>
2024-01-31 16:26:57 +0700

    feat: only allow existing token ids

--- server/api/preview-image.js
+++ server/api/preview-image.js
@@ -9,9 +9,14 @@ export default defineEventHandler(async (event) => {
 	const tokenID = query.token;
 	const imageType = query.type;
 
+	let fileType = 'jpg';
+	if (imageType === 'pfp') {
+		fileType = 'png';
+	}
+
 	const frameImage = html`
 	<div style="display: flex; width:100%; height:100%; background-color:#000;">
-		<img v-if="tokenID" src="http://images.hyperlootproject.com/${imageType}/${tokenID}.jpg" style="height: 100%; width: 100%; object-fit: contain">
+		<img v-if="tokenID" src="http://images.hyperlootproject.com/${imageType}/${tokenID}.${fileType}" style="height: 100%; width: 100%; object-fit: contain">
 	</div>`;
 	const svg = await satori(frameImage, {
 		width: 1200,
