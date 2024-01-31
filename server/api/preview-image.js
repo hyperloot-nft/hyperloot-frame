0e8e5185747a3a61cbd1e429e959611756c04082
Herin Kim <herin@hyperlootproject.com>
2024-01-31 19:36:53 +0700

    feat: add logo, id, bg img

--- server/api/preview-image.js
+++ server/api/preview-image.js
@@ -15,13 +15,30 @@ export default defineEventHandler(async (event) => {
 	}
 
 	const frameImage = html`
-	<div style="display: flex; width:100%; height:100%; background-color:#000;">
-		<img v-if="tokenID" src="http://images.hyperlootproject.com/${imageType}/${tokenID}.${fileType}" style="height: 100%; width: 100%; object-fit: contain">
-	</div>`;
+		<div style="display:flex; width:100%; height:100%; background-color:#000;">
+			<img src="http://images.hyperlootproject.com/${imageType}/${tokenID}.jpg" 
+     			style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover; z-index: -1; filter: blur(40px) brightness(80%);">
+			<img src="http://images.hyperlootproject.com/${imageType}/${tokenID}.${fileType}"
+				style="height: 100%; width: 100%; object-fit: contain">
+			<div style="z-index:100; position:absolute; top:20px; right:40px; font-family:'BluuNext-Bold'; font-size: 24px; color:#F3BA14;">#${tokenID}</div>
+			<img src="https://hyperloot-frame-preview-test.vercel.app/hyperloot-icon-transparent.png"
+				style="z-index:100; position:absolute; top:22px; left:30px; width:5%;">
+		</div>`;
+
+	const fontResponse = await fetch('https://hyperloot-frame-preview-test.vercel.app/BluuNext-Bold.otf');
+	const fontData = await fontResponse.arrayBuffer();
+
 	const svg = await satori(frameImage, {
 		width: 1200,
 		height: 600,
-		fonts: [],
+		fonts: [
+			{
+				name: 'BluuNext-Bold',
+				data: fontData,
+				weight: 400,
+				style: 'Bold',
+			},
+		],
 	});
 	const svgBuffer = Buffer.from(svg);
     const png = sharp(svgBuffer).png();
